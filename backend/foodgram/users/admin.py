02cbec81812a33601bfdc8f9b0a771fe08d3c541
Simon Egorov <rhinoRofl@gmail.com>
2022-08-11 22:39:05 +0700

    Configure admin site for users

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk',
+                    'username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'is_staff',
+                    'role',
+                    'confirmation_code',
+                    )
+    list_editable = ('username',
+                     'email',
+                     'first_name',
+                     'last_name',
+                     'is_staff',
+                     'role',
+                     'confirmation_code',
+                     )
+    search_fields = ('username',
+                     'email',
+                     'first_name',
+                     'last_name',
+                     )
+    list_filter = ('is_staff',
+                   'role',
+                   )
+    empty_value_display = 'empty'
+
+
+admin.site.register(User, UserAdmin)
