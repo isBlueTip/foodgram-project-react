0dfb157a632c2f0aea453a698f6681db84d43b03
Simon Egorov <rhinoRofl@gmail.com>
2022-08-14 13:34:49 +0700

    Create api urls

---
+++ backend/foodgram/api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import include, path
+
+from rest_framework.routers import SimpleRouter
+# from rest_framework.authtoken import views
+
+from api.views import RecipeViewSet, TagViewSet
+
+router = SimpleRouter()
+
+router.register('recipes', RecipeViewSet)
+router.register('tags', TagViewSet)
+# router.register('groups', GroupViewSet)
+# router.register(
+#     r'posts/(?P<post_id>\d+)/comments',
+#     CommentViewSet, basename='comment'
+# )
+
+
+api_patterns = [
+    # path('', include(router.urls)),
+    path('', include(router.urls)),
+    # path('v1/api-token-auth/', views.obtain_auth_token),
+]
+
+urlpatterns = [
+    path('', include(api_patterns)),
+]
