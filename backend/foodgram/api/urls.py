3c0d74b605160edee7b3322c13f2cff7a1df9aa6
Simon Egorov <rhinoRofl@gmail.com>
2022-08-16 18:07:14 +0700

    Add users endpoint

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -4,11 +4,13 @@ from rest_framework.routers import SimpleRouter
 # from rest_framework.authtoken import views
 
 from api.recipes_views import RecipeViewSet, TagViewSet
+from api.users_views import UserViewSet
 
 router = SimpleRouter()
 
 router.register('recipes', RecipeViewSet)
 router.register('tags', TagViewSet)
+router.register('users', UserViewSet)
 # router.register(
 #     r'posts/(?P<post_id>\d+)/comments',
 #     CommentViewSet, basename='comment'
