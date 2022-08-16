1eec63a0bbd31f5bfc7c11f2fca994befc77f5ae
Simon Egorov <rhinoRofl@gmail.com>
2022-08-16 18:06:52 +0700

    Add users endpoint

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings            # TODO dev settings
+from django.conf.urls.static import static  # TODO dev settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     # path('auth/', include('users.urls', namespace='users')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # TODO dev settings
