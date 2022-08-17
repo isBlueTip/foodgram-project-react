445166cf64d0f7d4ecafe840eb39c8c98db986df
Simon Egorov <rhinoRofl@gmail.com>
2022-08-17 11:16:29 +0700

    Update user model

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -10,6 +10,16 @@ class User(AbstractUser):
         (USER, 'User'),
         (ADMIN, 'Admin'),
     ]
+    first_name = models.CharField(
+        'first name',
+        max_length=150,
+        blank=False,
+    )
+    last_name = models.CharField(
+        'last name',
+        max_length=150,
+        blank=False,
+    )
     email = models.EmailField(
         'email address',
         unique=True,
