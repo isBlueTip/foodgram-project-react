f7c24f4664427df7df603db9b31460e2a1ba9327
Simon Egorov <rhinoRofl@gmail.com>
2022-10-28 11:27:12 +0600

    Update permissions

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -1,12 +1,14 @@
 from rest_framework.permissions import (SAFE_METHODS, BasePermission,
                                         IsAuthenticatedOrReadOnly)
+from users.models import ADMIN
 
 
-class RegisterOrIsAuthenticated(BasePermission):
+class IsAuthenticatedOrReadOnlyOrRegister(BasePermission):
 
     def has_permission(self, request, view):
         return bool(
             request.method == 'POST' or
+            request.method == SAFE_METHODS or
             request.user and
             request.user.is_authenticated
         )
@@ -17,5 +19,5 @@ class IsAdminOrIsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         return bool(request.method in SAFE_METHODS or
                     obj.author == request.user or
-                    request.user.is_staff
+                    request.user.role == ADMIN
                     )
