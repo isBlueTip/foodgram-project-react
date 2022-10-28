9310bc029171db016177773b01108e14d6a9eae0
Simon Egorov <rhinoRofl@gmail.com>
2022-10-28 12:54:21 +0600

    Create separate loggers

--- backend/foodgram/loggers.py
+++ backend/foodgram/loggers.py
@@ -1,7 +1,22 @@
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+
+logger_filters = logging.getLogger(__name__)
+logger_filters.setLevel(logging.DEBUG)
+
+logger_recipe_serializers = logging.getLogger(__name__)
+logger_recipe_serializers.setLevel(logging.DEBUG)
+
+logger_recipe_views = logging.getLogger(__name__)
+logger_recipe_views.setLevel(logging.DEBUG)
+
+logger_users_serializers = logging.getLogger(__name__)
+logger_users_serializers.setLevel(logging.DEBUG)
+
+logger_users_views = logging.getLogger(__name__)
+logger_users_views.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter(
     '%(asctime)s | %(name)s | %(levelname)s | %(message)s')
