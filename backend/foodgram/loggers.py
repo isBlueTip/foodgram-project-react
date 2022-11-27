9c47d33a505fe8e2a57b07ed0705f8bc15467783
Simon Egorov <rhinoRofl@gmail.com>
2022-11-27 19:20:02 +0600

    Add logger for tests

--- backend/foodgram/loggers.py
+++ backend/foodgram/loggers.py
@@ -15,5 +15,8 @@ logger_users_serializers.setLevel(logging.DEBUG)
 logger_users_views = logging.getLogger(__name__)
 logger_users_views.setLevel(logging.DEBUG)
 
+logger_tests = logging.getLogger(__name__)
+logger_tests.setLevel(logging.DEBUG)
+
 formatter = logging.Formatter("%(asctime)s | %(name)s |"
                               " %(levelname)s | %(message)s")
