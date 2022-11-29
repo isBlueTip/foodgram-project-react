61cb7424c2dcc9ba14732079de74445844407c38
Simon Egorov <rhinoRofl@gmail.com>
2022-11-29 17:17:12 +0600

    Refactor 'test_urls' to specific function

--- backend/foodgram/tests/test_urls.py
+++ backend/foodgram/tests/test_urls.py
@@ -1,59 +1,74 @@
 import logging
+from http import HTTPStatus
 
 from rest_framework import test
 
+from recipes.models import Ingredient, Recipe, Tag
 from tests import fixtures
 from users.models import User
-from recipes.models import Recipe, Tag, Ingredient
-from loggers.loggers import formatter, logger_tests
 
-LOG_NAME = "logs/logger_tests.log"
+logger = logging.getLogger("logger")
 
-file_handler = logging.FileHandler(LOG_NAME)
-file_handler.setFormatter(formatter)
-logger_tests.addHandler(file_handler)
+
+def test_urls_list(self, urls_list, client, response_code):
+    for url in urls_list:
+        with self.subTest(url=url):
+            response = client.get(url)
+            test.APITestCase.assertEqual(self, response.status_code,
+                                         response_code)
 
 
 class URLsTest(test.APITestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = User.objects.create(username='HasNoName')
+        cls.user = User.objects.create(username="HasNoName")
         cls.guest_client = test.APIClient()
         cls.authenticated_client = test.APIClient()
         cls.authenticated_client.force_authenticate(cls.user)
-        recipe = Recipe.objects.create(name=fixtures.recipe_name,
-                                       author=URLsTest.user,
-                                       cooking_time=fixtures.cooking_time)
+        recipe = Recipe.objects.create(
+            name=fixtures.recipe_name,
+            author=URLsTest.user,
+            cooking_time=fixtures.cooking_time,
+        )
         tag = Tag.objects.create(slug=fixtures.tag_1_slug)
-        ingredient = Ingredient.objects.create(name=fixtures.ingredient_1_name)
+        ingredient = Ingredient.objects.create(
+            name=fixtures.ingredient_1_name)
         cls.authenticated_urls = fixtures.authenticated_urls + [
-            '/api/users/' + str(cls.user.id) + '/',
+            "/api/users/" + str(cls.user.id) + "/",
         ]
         cls.unauthenticated_urls = fixtures.unauthenticated_urls + [
-            '/api/tags/' + str(tag.id) + '/',
-            '/api/recipes/' + str(recipe.id) + '/',
-            '/api/ingredients/' + str(ingredient.id) + '/',
+            "/api/tags/" + str(tag.id) + "/",
+            "/api/recipes/" + str(recipe.id) + "/",
+            "/api/ingredients/" + str(ingredient.id) + "/",
         ]
 
     def test_urls_for_authenticated(self):
-        for url in fixtures.authenticated_urls:
-            with self.subTest(url=url):
-                response = URLsTest.authenticated_client.get(url)
-                self.assertEqual(response.status_code, 200)
+        test_urls_list(
+            self,
+            fixtures.authenticated_urls,
+            URLsTest.authenticated_client,
+            HTTPStatus.OK,
+        )
 
-        for url in URLsTest.unauthenticated_urls:
-            with self.subTest(url=url):
-                response = URLsTest.authenticated_client.get(url)
-                self.assertEqual(response.status_code, 200)
+        test_urls_list(
+            self,
+            URLsTest.unauthenticated_urls,
+            URLsTest.authenticated_client,
+            HTTPStatus.OK,
+        )
 
     def test_urls_for_guest(self):
-        for url in fixtures.authenticated_urls:
-            with self.subTest(url=url):
-                response = URLsTest.guest_client.get(url)
-                self.assertEqual(response.status_code, 401)
+        test_urls_list(
+            self,
+            fixtures.authenticated_urls,
+            URLsTest.guest_client,
+            HTTPStatus.UNAUTHORIZED,
+        )
 
-        for url in URLsTest.unauthenticated_urls:
-            with self.subTest(url=url):
-                response = URLsTest.guest_client.get(url)
-                self.assertEqual(response.status_code, 200)
+        test_urls_list(
+            self,
+            URLsTest.unauthenticated_urls,
+            URLsTest.guest_client,
+            HTTPStatus.OK
+        )
