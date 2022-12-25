b6dde3c4cb5aa967fb06202ff8a988bdfa655e14
Simon Egorov <rhinoRofl@gmail.com>
2022-12-25 22:07:08 +0600

    Add dev settings to root urls

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,3 +1,5 @@
+from foodgram.settings import settings_dev
+
 from django.conf import settings  # TODO dev settings
 from django.conf.urls.static import static  # TODO dev settings
 from django.contrib import admin
@@ -6,6 +8,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
-] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)  # TODO dev settings
+]
+
+if not settings_dev.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
