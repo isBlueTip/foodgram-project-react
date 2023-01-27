ec0cd01f9bd47dffb8f8f456bf912deefe56f2cb
Simon Egorov <rhinoRofl@gmail.com>
2023-01-27 13:30:40 +0600

    Add custom paginator

--- backend/foodgram/foodgram/settings/settings_prod.py
+++ backend/foodgram/foodgram/settings/settings_prod.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "api.paginators.CartPaginator",
     "PAGE_SIZE": 6,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
